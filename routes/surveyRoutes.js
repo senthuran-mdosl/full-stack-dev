a0bf2e18ae20f2348a4a46141e0323736ccfc454
Senthuran <senthuran@mydigitaloffice.ca>
2021-05-24 12:48:00 +0530

    sendgrid backend full

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -8,6 +8,9 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+    app.get('/api/surveys/thanks', (req,res) => {
+        res.send('Thanks for voting!');
+    });
     app.post('/api/surveys', requireLogin, requireCredits, async (res,req) => {
         const { title, subject, body, recipients } = req.body;
 
@@ -21,6 +24,16 @@ module.exports = app => {
         });
 
         const Mailer = new Mailer(survey, surveyTemplate(survey));
+
+        try{
+        await mailer.send();
+        await survey.save();
+        req.user.credits -=1;
+        const user = await req.user.save();
+        res.send(user);
+        } catch (err) {
+            res.status(422).send(err);
+        }
     });
 
 };
--- services/Mailer.js
+++ services/Mailer.js
@@ -6,18 +6,48 @@ class Mailer extends helper.Mail {
     constructor({ subject, recipients }, content){
         super();
 
+        this.sgApi = sendgrid(keys.sendGridKey);
         this.from_email = new helper.Email('no-reply@emaily.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
         this.recipients = this.formatAddresses(recipients);
 
         this.addContent(this.body);
+        this.addClickTracking();
+        this.addRecipients();
     }
 
     formatAddresses(recipients){
         return recipients.map(({ email }) => {
             return new helper.Email(email);
         });
+    }
+
+    addClickTracking() {
+        const trackSettings = new helper.trackSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
+
+        trackSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackSettings)
+    }
+
+    addRecipients(){
+        const personalize = new helper.Personalization();
+        this.recipients.forEach(recipient => {
+            personalize.addTo(recipient);
+        });
+        this.addPersonalization(personalize);
+    }
+
+    async send() {
+        const request = this.sgApi.emptyRequest({
+            method: 'POST',
+            path: 'v3/mail/send',
+            body: this.toJSON()
+        });
+
+        const response = this.sgApi.API(request);
+        return response;
     }
 };
 
