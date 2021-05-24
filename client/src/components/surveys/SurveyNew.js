e06519860280495aafee5a13f049edd3577d0534
Senthuran <senthuran@mydigitaloffice.ca>
2021-05-24 13:17:49 +0530

    redux form basics

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,13 @@
+import React, { Compomenent } from 'react';
+
+class SurveyNew extends Component {
+    render() {
+        return (
+            <div>
+                SurveyNew!
+            </div>
+        );
+    }
+}
+
+export default SurveyNew;
