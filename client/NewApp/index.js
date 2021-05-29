2f28ac99c749ab2cb560a385282d4f975e61f228
Senthuran <senthuran@mydigitaloffice.ca>
2021-05-29 10:48:23 +0530

    nodejs server http

--- client/NewApp/index.js
+++ client/NewApp/index.js
@@ -1,4 +1,10 @@
 const { sum } = require("./helpers");
+const http = require('http');
+const server = http.createServer((req,res) => {
+    res.end("hello world");
+});
+
+server.listen(3000);
 
 const total = sum(10,200);
 
