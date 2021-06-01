0c51af5f5568a886ab9acd1ad0b487dcbbb19404
Senthuran <senthuran@mydigitaloffice.ca>
2021-06-01 17:41:00 +0530

    logout revert to original

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,8 +14,8 @@ module.exports = app => {
         );
 
     app.get('/api/logout', (res,req) => {
-        res.redirect('/');
         req.logout();
+        res.redirect('/');
     });
 
     app.get ('/api/current_user', (req,res) => {
